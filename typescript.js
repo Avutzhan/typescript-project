14c136da556b02b03c6e7da7e779db78e0250d1b
Avutzhan Dautov <dautov92@list.ru>
2021-05-03 09:12:25 +0600

    TypeScript 4

--- typescript.js
+++ typescript.js
@@ -1,4 +1,29 @@
-var sum = function (a, b) {
-    return a + b;
+//boolean
+var isCool = true;
+//number
+var age = 56;
+//string
+var eyeColor = 'brown';
+var favouriteQuote = "im not old im only  " + age;
+//array
+var pets = ['cat', 'dog', 'pig'];
+// let pets2 = Array<string> = ['lion', 'dragon', 'lizard']; probably deprecated
+//object
+var wizard = {
+    a: 'John'
 };
-sum(1, 2);
+//null and undefined
+var meh = undefined;
+var noo = null;
+//tuple
+//basket expecting this specific type and order
+var basket;
+basket = ['basketball', 5];
+//enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size.Small;
--- typescript.ts
+++ typescript.ts
@@ -1,5 +1,36 @@
-const sum = (a: number, b: number) => {
-    return a + b;
+//boolean
+let isCool: boolean = true
+
+//number
+let age: number = 56;
+
+//string
+let eyeColor: string = 'brown';
+let favouriteQuote: string = "im not old im only  " + age;
+
+//array
+let pets: string[] = ['cat', 'dog', 'pig'];
+// let pets2 = Array<string> = ['lion', 'dragon', 'lizard']; probably deprecated
+
+//object
+let wizard: object = {
+    a: 'John'
 }
 
-sum(1, 2)
+//null and undefined
+let meh: undefined = undefined
+let noo: null = null
+
+//tuple
+//basket expecting this specific type and order
+let basket: [string, number];
+basket = ['basketball', 5]
+
+//enum
+enum Size {
+    Small = 1,
+    Medium = 2,
+    Large = 3
+}
+
+let sizeName: number = Size.Small;
